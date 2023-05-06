eea72a3e84690eb8b46dc80bc8f2470321f303c1
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-05-06 20:54:20 -0300

    feat(login): login screen interaction with google authentication

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,32 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../../services/firebaseconfig";
+
+const provider = new GoogleAuthProvider();
+
 export const Login = () => {
+  const auth = getAuth(app);
+  const signInGoogle = () => {
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        console.log(user)
+
+      }).catch((error) => {
+
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+
+      });
+  }
+
   return (
     <div>
-      <h1>Login</h1>
+      <button onClick={signInGoogle}>Logar com Google</button>
     </div>
   );
 }
