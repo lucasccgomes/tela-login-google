9733186ff65c413f383ab6f86be3365ae1a6b150
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-05-08 10:32:21 -0300

    feat(home): simple environment to receive username and logout button
    n

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+import { AuthGoogleContext } from "../../context/authGoogle";
+
 export const Home = () => {
+  const { user, signOut } = useContext(AuthGoogleContext)
+  let userLogado = JSON.parse(user)
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Bem Vindo {userLogado.displayName}</h1>
+      <button onClick={() => signOut()} >Sair</button>
     </div>
   );
 }
